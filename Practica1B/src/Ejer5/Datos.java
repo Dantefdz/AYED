97c482121a26d9d06d65685beb4bca3ee4bb1ec6
Dante fernandez <88099408+Dantefdz@users.noreply.github.com>
2022-03-23 23:03:49 -0300

    Update Datos.java

--- Practica1B/src/Ejer5/Datos.java
+++ Practica1B/src/Ejer5/Datos.java
@@ -1,31 +1,28 @@
 package Ejer5;
 
+
+
 public class Datos {
-	private int min;
-	private int max;
-	private float prom;
-
-	public int getMin() {
-		return min;
+     private int Max;
+     private int Min;
+     private int Pro;
+     
+     public void setMax(int max) {
+		Max = max;
 	}
-
-	public void setMin(int min) {
-		this.min = min;
+    public void setMin(int min) {
+		Min = min;
 	}
-
-	public int getMax() {
-		return max;
+    public void setPro(int pro) {
+		Pro = pro;
 	}
-
-	public void setMax(int max) {
-		this.max = max;
+    public int getMax() {
+		return Max;
 	}
-
-	public void setProm(float prom) {
-		this.prom = prom;
+    public int getMin() {
+		return Min;
 	}
-
-	public float getProm() {
-		return prom;
+    public int getPro() {
+		return Pro;
 	}
 }
